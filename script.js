9b4b922392e7f155cd7b77b167876147ebf5351d
Nick Gertler <nickgertler@gmail.com>
2023-03-12 11:28:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+/* Fade title section on scroll */
+// Get the title element
+// Targets the network-background DIV to allow for fixed-position scroll
+const title = document.querySelector('#logo');
+const nav = document.querySelector('.menu-icon')
+
+// Get the viewport height
+const viewportHeight = window.innerHeight;
+
+// Add a scroll event listener to the window
+window.addEventListener('scroll', function() {
+  // Calculate the opacity based on the scroll position
+  const opacity = 1 - Math.min(window.scrollY / (viewportHeight * 0.8), 1);
+
+  // Set the opacity of the title element
+  title.style.opacity = opacity;
+
+  // Check if the user has scrolled more than 80% of the viewport
+  if (window.scrollY > viewportHeight * 0.8) {
+    title.style.opacity = 0;
+    nav.style.visibility = 'visible';
+  } else {
+    title.style.opacity = opacity;
+    nav.style.visibility = 'hidden';
+  }
+});
+
+/* Smooth scrolling for anchor links */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+/* Change welcome navigation bar container on resize */
+const container = document.querySelector('.nav-select-grid'); // Select the container element
+const content = document.querySelector('.nav-select-grid-item')
+const contentText = document.querySelector('.nav-select-grid-item h3')
+
+function checkContainerWidth() {
+  const width = window.innerWidth; // Get the width of the viewport
+
+  if (width < 516) { // Check if the width is less than 516px. This seems to be the breakpoint.
+    container.classList.add('rows');
+    content.classList.add('rows');
+    contentText.classList.add('rows');
+  } else {
+    container.classList.remove('rows');
+    content.classList.remove('rows');
+    contentText.classList.remove('rows');
+  }
+}
+
+// Call the function initially to set the correct class
+checkContainerWidth();
+
+// Call the function on resize
+window.addEventListener('resize', checkContainerWidth);
+
+
+/* Control nav bar */
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+const menuClose = document.querySelector('.menu__close');
+
+menuIcon.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+});
+
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('menu--open');
+});
+
+document.addEventListener('click', (event) => {
+  const isClickInside = menu.contains(event.target) || menuIcon.contains(event.target);
+  if (!isClickInside && menu.classList.contains('menu--open')) {
+    menu.classList.remove('menu--open');
+  }
+});
+
+document.querySelectorAll('.menu li a').forEach(item => {
+  item.addEventListener('click', () => {
+    menu.classList.remove('menu--open');
+  });
+});
